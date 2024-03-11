12be2d1f871e581f4c7ea0965f070d884309ae50
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2024-03-10 21:21:12 -0600

    agua de jamaica

--- GaleriaDeFotosServer/Views/LaOtraView.xaml.cs
+++ GaleriaDeFotosServer/Views/LaOtraView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GaleriaDeFotosServer
+namespace GaleriaDeFotosServer.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para LaOtraView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LaOtraView : Window
     {
-        public MainWindow()
+        public LaOtraView()
         {
             InitializeComponent();
         }
